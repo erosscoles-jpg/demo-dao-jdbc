98c1006959ecb692f871a15b3b427165a827909c
Eros Scoles <eros.scoles@gmail.com>
2026-06-18 15:39:04 -0300

    Delete creted

--- src/aplication/Program2.java
+++ src/aplication/Program2.java
@@ -2,6 +2,7 @@ package aplication;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -10,6 +11,7 @@ import model.entites.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		DepartmentDao depDao = DaoFactory.createDepartmentDao();
 		
@@ -22,12 +24,26 @@ public class Program2 {
 		Department dep = depDao.findById(2); 
 		System.out.println(dep);
 		
-		System.out.println("\n=== Test 2 : Seller FindById ===== ");
+		System.out.println("\n=== Test 3 : Seller FindById ===== ");
 		List<Department> list = new ArrayList<>();
 		list = depDao.findAll();
 		
 		for (Department obj: list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== Test 4 : Seller Insert ===== ");
+		dep = depDao.findById(1);
+		dep.setName("RockStar");
+		depDao.uptade(dep);
+		System.out.println("Upatade Completed");
+		
+		System.out.println("\n=== Test 5 : Seller Insert ===== ");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		depDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 	}
 }
